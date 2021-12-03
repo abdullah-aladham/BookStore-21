d1c26069b4db44fca0d713daf090c61850862321
Abdullah Aladham <abdullah.aladham@gmail.com>
2021-12-03 21:07:13 +0200

    3/12/2021 commit

---
+++ BookStore/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string description { get; set; }
+        public Author b_Autohr { get; set; }
+    }
+}
---
+++ BookStore/Repositories/IBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Repositories
+{
+    interface IBook<TEntity>
+    {
+        IList<TEntity> List();
+        TEntity Find(int id);
+        void Add(TEntity entity);
+    }
+}
